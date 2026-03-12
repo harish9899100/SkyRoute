61cd85490fd0d31f792abb5b3d8bc911e24d9749
harish khileri <harishkhileri98@gmail.com>
2026-03-12 17:06:32 +0530

    add Google-OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    account = User.where(email: data['email']).first
+    account ||= User.create(name: data['name'], email: data['email'], password: Devise.friendly_token[0, 20])
+    account
+  end
 end
