2f1c69db3e2844b86e532861f1c7abfc5b98cddb
harish khileri <harishkhileri98@gmail.com>
2026-08-13 16:51:44 +0530

    update ui and use google auth for sign-in sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable
+         :omniauthable, :omniauth_providers => [:google_oauth2]
 
   def self.from_omniauth(access_token)
     data = access_token.info
